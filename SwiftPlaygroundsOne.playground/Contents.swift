d297f05561d82410c32f55ca94d31acae6f45df8
CM Student <cmstudent@UHWO-ACM-C-05.local>
2018-01-22 11:23:10 -1000

    Finished SwiftPlaygrounds #1 Assignment

--- SwiftPlaygroundsOne.playground/Contents.swift
+++ SwiftPlaygroundsOne.playground/Contents.swift
@@ -1,6 +1,14 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
-var str = "Hello, playground"
 //commenthere
+var myFirstVariable: String
+myFirstVariable = "I've created my first variable"
+
+//we're going to do something with strings
+var mySecondVariable = " and look i added two strings together"
+
+myFirstVariable + mySecondVariable
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+print(myThirdVariable)
